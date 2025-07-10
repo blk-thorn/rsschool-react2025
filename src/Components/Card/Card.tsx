d6812e8cee12a289719aa3241ef7ffdfbeede9d4
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-11 01:06:13 +0300

    style: change character cards

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -3,23 +3,42 @@ import { Component } from 'react';
 class Card extends Component {
   render() {
     return (
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-500 dark:border-gray-600 overflow-hidden">
-        <article
-          className="relative isolate flex flex-col justify-end px-8 pb-8 pt-40 max-w-sm mx-auto">
-          <img src="./public/peakpx.jpg" alt="character" className="absolute inset-0 h-full w-full object-cover" />
-          <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-          <h3 className="z-10 mt-5 text-2xl font-bold text-white">Status: Alive</h3>
-        </article>
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Some character</h5>
-          <ul className="mb-3 font-normal text-gray-400 dark:text-gray-200 text-left">
-            <li>Species:</li>
-            <li>Last known location:</li>
-            <li>First seen in:</li>
-          </ul>
+      <div className="flex max-w-md bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-600 dark:border-gray-600 overflow-hidden">
+        <div className="relative w-1/3 min-w-[120px]">
+          <img
+            className="w-full h-full object-cover min-h-[140px]"
+            src="https://rickandmortyapi.com/api/character/avatar/353.jpeg"
+            alt="Tiny Rick"
+          />
+          <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-2">
+            <h3 className="text-white text-sm font-bold truncate">Tiny Rick</h3>
+          </div>
+        </div>
+
+        <div className="w-2/3 p-2">
+          <table className="w-full border-collapse">
+            <tbody>
+            <tr className="border border-gray-200 dark:border-gray-400">
+              <td className="py-1 pr-2 font-medium text-gray-400 dark:text-gray-300 border-r border-gray-200 dark:border-gray-400">Status: </td>
+              <td className="py-1 pl-2 text-gray-600 dark:text-gray-400">Dead</td>
+            </tr>
+            <tr className="border border-gray-200 dark:border-gray-400">
+              <td className="py-1 pr-2 font-medium text-gray-400 dark:text-gray-300 border-r border-gray-200 dark:border-gray-400">Species: </td>
+              <td className="py-1 pl-2 text-gray-600 dark:text-gray-400">Human</td>
+            </tr>
+            <tr className="border border-gray-200 dark:border-gray-400">
+              <td className="py-1 pr-2 font-medium text-gray-400 dark:text-gray-300 border-r border-gray-200 dark:border-gray-400">Location: </td>
+              <td className="py-1 pl-2 text-gray-600 dark:text-gray-400">Earth (Replacement Dimension)</td>
+            </tr>
+            <tr className="border border-gray-200 dark:border-gray-400">
+              <td className="py-1 pr-2 font-medium text-gray-400 dark:text-gray-300 border-r border-gray-200 dark:border-gray-400">First seen: </td>
+              <td className="py-1 pl-2 text-gray-600 dark:text-gray-400">Big Trouble in Little Sanchez</td>
+            </tr>
+            </tbody>
+          </table>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -4,7 +4,7 @@ import Card from '@/Components/Card/Card.tsx';
 class Gallery extends Component {
   render() {
     return (
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid gap-2 mt-8 min-sm:grid-cols-2 min-lg:grid-cols-3 min-xl:grid-cols-4 animate-fadeIn gap-2">
         <Card />
         <Card />
         <Card />
