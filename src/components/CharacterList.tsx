5ae29e1fef44e28b0f9ec1db9138609d474e051a
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-14 16:18:16 +0300

    chore: add optional dependencies

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -7,13 +7,13 @@ import { CharactersListProps } from '@/types/types.ts';
 class CharactersList extends Component<CharactersListProps> {
   render(): ReactElement {
     const { characters, searchTerm, totalPages, currentPage, onPageChange } = this.props;
-    const showNotFound = characters.length === 0 && searchTerm !== '';
-    const showPagination = totalPages > 0;
+    const showNotFound= characters.length === 0 && searchTerm !== '';
+    const showPagination= totalPages > 0;
 
     return (
       <>
         <div className="grid gap-4 mt-8 min-sm:grid-cols-2 min-lg:grid-cols-3 min-xl:grid-cols-4 animate-fadeIn justify-center">
-          {characters.map((character) => (
+          {characters.map((character)  => (
             <Card key={character.id} character={character} />
           ))}
           <NotFoundMessage searchTerm={searchTerm} show={showNotFound} />
