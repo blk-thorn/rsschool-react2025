633f31c402cdb7039cd2de7a83fa6045b807b5c9
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-14 23:18:20 +0300

    fix: add missing types

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -2,18 +2,18 @@ import { Component, ReactElement } from 'react';
 import Card from '@/components/Card';
 import NotFoundMessage from '@/components/NotFoundMessage';
 import Pagination from '@/components/Pagination';
-import { CharactersListProps } from '@/types/types.ts';
+import { Character, CharactersListProps } from '@/types/types.ts';
 
 class CharactersList extends Component<CharactersListProps> {
   render(): ReactElement {
     const { characters, searchTerm, totalPages, currentPage, onPageChange } = this.props;
-    const showNotFound= characters.length === 0 && searchTerm !== '';
-    const showPagination= totalPages > 0;
+    const showNotFound: boolean= characters.length === 0 && searchTerm !== '';
+    const showPagination: boolean= totalPages > 0;
 
     return (
       <>
         <div className="grid gap-4 mt-8 min-sm:grid-cols-2 min-lg:grid-cols-3 min-xl:grid-cols-4 animate-fadeIn justify-center">
-          {characters.map((character)  => (
+          {characters.map((character: Character): ReactElement  => (
             <Card key={character.id} character={character} />
           ))}
           <NotFoundMessage searchTerm={searchTerm} show={showNotFound} />
