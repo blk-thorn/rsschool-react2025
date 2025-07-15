baafab380739e310c41bd2fbf260b68006ac5a07
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-16 02:54:27 +0300

    fix: home page path, husky and card theme

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -23,14 +23,14 @@ class Card extends Component<CardProps> {
     const statusColorClass: string = this.getStatusColor(character.status);
 
     return (
-      <div className="flex items-center max-w-xl bg-white border border-slate-200 rounded-lg shadow-sm dark:bg-slate-600 dark:border-slate-600 overflow-hidden">
+      <div className="flex items-center max-w-xl bg-slate-600 border border-slate-200 rounded-lg shadow-sm dark:border-slate-600 overflow-hidden">
         <div className="relative w-3/4 min-w-120px">
           <img
-            className="w-full h-full object-cover min-h-50"
+            className="w-full h-full object-cover min-h-58 rounded-lg min-w-33"
             src={character.image}
             alt="Tiny Rick"
           />
-          <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-2">
+          <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-2 rounded-lg">
             <h3 className="text-white text-md font-bold truncate">
               {character.name}
             </h3>
@@ -43,11 +43,11 @@ class Card extends Component<CardProps> {
               {character.status}
             </span>
           </li>
-          <li className="mb-1 flex">
+          <li className="mb-1 flex flex-col">
             <span className="block text-white font-medium">Gender:</span>
             <span className="text-slate-300 mx-2">{character.gender}</span>
           </li>
-          <li className="mb-1 flex">
+          <li className="mb-1 flex flex-col">
             <span className="block text-white font-medium">Species:</span>
             <span className="text-slate-300 mx-2">{character.species}</span>
           </li>
