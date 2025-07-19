3cc73e62ed8ca66bea75e24377771e6deff9378b
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-20 02:53:14 +0300

    feat: add HomePage component tests

---
+++ src/Pages/HomePage/HomePage.test.tsx
@@ -0,0 +1,101 @@
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { describe, it, expect, beforeEach } from 'vitest';
+import { vi } from 'vitest';
+import HomePage from './HomePage';
+import { ApiResponse } from '@/types/types.ts';
+import { fetchCharacters } from '@/utils/api';
+
+type ConsoleSpy = {
+  mockImplementation: (fn: () => void) => void;
+  mockRestore: () => void;
+};
+
+vi.mock('@/utils/api');
+
+describe('HomePage', (): void => {
+  beforeEach((): void => {
+    vi.mocked(fetchCharacters).mockResolvedValue({
+      info: { count: 1, pages: 1, next: null, prev: null },
+      results: [
+        {
+          id: 1,
+          name: 'Rick Sanchez',
+          status: '',
+          species: '',
+          type: '',
+          gender: '',
+          origin: {
+            name: '',
+            url: '',
+          },
+          location: {
+            name: '',
+            url: '',
+          },
+          image: '',
+          episode: [],
+          url: '',
+          created: '',
+        },
+      ],
+    });
+  });
+
+  it('check loading characters on mount', async (): Promise<void> => {
+    render(<HomePage />);
+    await waitFor((): void => {
+      expect(fetchCharacters).toHaveBeenCalled();
+    });
+  });
+
+  it('check loading state', (): void => {
+    vi.mocked(fetchCharacters).mockImplementation((): Promise<ApiResponse> =>
+      new Promise((): void => {})
+    );
+    render(<HomePage />);
+    expect(screen.queryByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('check error state', async (): Promise<void> => {
+    const consoleSpy: ConsoleSpy = vi.spyOn(console, 'error').mockImplementation((): void => {});
+    vi.mocked(fetchCharacters).mockRejectedValue(new Error('API Error'));
+
+    render(<HomePage />);
+    await waitFor((): void => {
+      expect(screen.queryByText('Rick Sanchez')).not.toBeInTheDocument();
+    });
+
+    consoleSpy.mockRestore();
+  });
+
+  it('display footer', async (): Promise<void> => {
+    render(<HomePage />);
+    await waitFor((): void => {
+      expect(screen.getByRole('contentinfo')).toBeInTheDocument();
+    });
+  });
+
+  it('check search functionality', async (): Promise<void> => {
+    render(<HomePage />);
+
+    const searchInput: HTMLElement = screen.getByPlaceholderText('Search a character...');
+    const searchButton: HTMLElement = screen.getByRole('button', { name: /search/i });
+
+    fireEvent.change(searchInput, { target: { value: 'Rick' } });
+    fireEvent.click(searchButton);
+
+    await waitFor((): void => {
+      expect(fetchCharacters).toHaveBeenCalledWith('Rick', 1);
+      expect(localStorage.getItem('searchTerm-the-rick-morty-api')).toBe('Rick');
+    });
+  });
+
+  it('throw error when ErrorButton is clicked', async (): Promise<void> => {
+    render(<HomePage />);
+
+    const errorButton: HTMLElement = await screen.findByRole('button', { name: /Error Button/i });
+
+    expect(errorButton).toBeInTheDocument();
+    expect((): boolean => fireEvent.click(errorButton)).toThrow('Something went wrong. Please reload the page.');
+  });
+});
