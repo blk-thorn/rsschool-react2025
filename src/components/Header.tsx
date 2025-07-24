f49e9f1d49a9a8cad64c7f97d5c97384a4aaafbf
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-25 00:45:25 +0300

    refactor: convert Header to functional component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,10 @@
-import { Component, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
-class Header extends Component{
-  render(): ReactNode {
+export default function Header(): ReactNode {
     return (
       <header className="flex flex-col items-center justify-center">
-        <img src="./favicon.ico" alt="logo" className="rounded-full w-40 h-40 border-5" ></img>
+        <img src="/favicon.ico" alt="logo" className="rounded-full w-40 h-40 border-5" ></img>
         <h1 className="text-center mt-8 mb-8 font-bold text-5xl text-sky-600">The Rick and Morty API</h1>
       </header>
     )
-  }
 }
-
-export default Header;
