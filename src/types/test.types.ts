93b8483a3cc3fb9a8d64f4b2b823a5df8b86cef8
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-24 22:06:20 +0300

    feat: add pre-push hook

--- src/types/test.types.ts
+++ src/types/test.types.ts
@@ -1,6 +1,6 @@
 import { RenderResult } from '@testing-library/react';
+import { ReactElement, ReactNode } from 'react';
 import type { Mock } from 'vitest';
-import { ReactElement } from 'react';
 
 export type ConsoleError = {
   (...data: []): void;
@@ -13,4 +13,4 @@ export type renderFunction =  () => RenderResult
 export type searchBarRender = ( term?: string, mockFn?: Mock) => RenderResult
 
 
-export type childComponent = ({ children }: { children: React.ReactNode }) => ReactElement  | null
+export type childComponent = ({ children }: { children: ReactNode }) => ReactElement  | null
