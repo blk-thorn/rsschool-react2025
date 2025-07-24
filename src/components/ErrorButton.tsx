1fe92ee6faa2e18daa12377c431056b341d8137b
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-25 00:30:40 +0300

    refactor: convert ErrorButton to functional component

--- src/components/ErrorButton.tsx
+++ src/components/ErrorButton.tsx
@@ -1,22 +1,19 @@
-import { Component, ReactElement } from 'react';
+import { ReactNode } from 'react';
 import { ClickEvent, ClickFunction, ErrorButtonProps } from '@/types/types.ts';
 
-class ErrorButton extends Component<ErrorButtonProps> {
-  handleClick: ClickFunction = (e: ClickEvent): void => {
+export default function ErrorButton({onErrorClick}: ErrorButtonProps): ReactNode  {
+
+  const handleClick: ClickFunction = (e: ClickEvent): void => {
     e.preventDefault();
-    this.props.onErrorClick();
+    onErrorClick();
   }
 
-  render(): ReactElement {
     return (
       <button
-        onClick={this.handleClick}
+        onClick={handleClick}
         type="button"
         className="focus:outline-none text-gray-50 bg-rose-600 hover:bg-rose-700 focus:ring-1 focus:ring-rose-300 font-medium rounded-lg text-md px-20 py-3 me-2 mb-2 dark:bg-rose-500 dark:hover:bg-rose-600 dark:focus:ring-rose-600 float-right cursor-pointer">
         Error Button
       </button>
     )
-  }
-}
-
-export default ErrorButton;
+};
