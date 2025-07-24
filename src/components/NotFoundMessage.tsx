bde3ff1b4b5816086cdbc300bf06477bcc1e40f0
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-25 01:15:00 +0300

    refactor: convert NotFoundMessage to functional component

--- src/components/NotFoundMessage.tsx
+++ src/components/NotFoundMessage.tsx
@@ -1,10 +1,8 @@
-import { Component, ReactElement } from 'react';
+import { ReactNode } from 'react';
 import { NotFoundMessageProps } from '@/types/types.ts';
 
-class NotFoundMessage extends Component<NotFoundMessageProps> {
-  render (): ReactElement | null {
-    const { searchTerm } = this.props;
-    if (!this.props.show) return null;
+export default function NotFoundMessage({searchTerm, show}: NotFoundMessageProps): ReactNode | null {
+    if (!show) return null;
     return (
       <div className="col-span-full text-center py-10">
         <p className="text-gray-500 text-lg">
@@ -12,7 +10,4 @@ class NotFoundMessage extends Component<NotFoundMessageProps> {
         </p>
       </div>
     )
-  }
-}
-
-export default NotFoundMessage;
+};
