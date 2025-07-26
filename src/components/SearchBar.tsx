2f7525d43756b8dfdc2d7f3300bed234093abe98
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-26 19:22:41 +0300

    refactor: convert SearchBar to functional component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,37 +1,30 @@
-import { Component, ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { ChangeEvent, ChangeFunction, SearchProps, SubmitEvent, SubmitFunction } from '@/types/types.ts';
 
-class SearchBar extends Component<SearchProps> {
-  state: {term: string} = {
-    term: this.props.initialSearchTerm
-  };
+export default function SearchBar({initialSearchTerm, onFormSubmit}: SearchProps ): ReactNode{
 
+  const [term, setTerm] = useState(initialSearchTerm);
 
-  componentDidUpdate(prevProps: SearchProps): void {
-    if (prevProps.initialSearchTerm !== this.props.initialSearchTerm) {
-      this.setState({ term: this.props.initialSearchTerm });
-    }
+  useEffect((): void => {
+    setTerm(initialSearchTerm);
+  }, [initialSearchTerm]);
+
+  const handleInputChange: ChangeFunction = (e: ChangeEvent): void => {
+    setTerm(e.target.value);
   }
 
-  onInputChange: ChangeFunction = (e: ChangeEvent): void => {
-    this.setState({term: e.target.value});
-  }
-
-  onFormSubmit: SubmitFunction = (e:SubmitEvent): void => {
+  const handleFormSubmit: SubmitFunction = (e:SubmitEvent): void => {
     e.preventDefault();
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   }
-
-
-  render(): ReactNode {
 
     return (
-      <form onSubmit={this.onFormSubmit} className="flex items-center max-w-lg mx-auto mb-10">
+      <form onSubmit={handleFormSubmit} className="flex items-center max-w-lg mx-auto mb-10">
         <label htmlFor="search-bar" className="sr-only">Search</label>
         <div className="relative w-full">
           <input type="search" id="search-bar"
-                 value={this.state.term}
-                 onChange={this.onInputChange}
+                 value={term}
+                 onChange={handleInputChange}
                  className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full ps-5 p-2.5"
                  placeholder="Search a character..."/>
         </div>
@@ -46,8 +39,4 @@ class SearchBar extends Component<SearchProps> {
         </button>
       </form>
     )
-  }
-
-}
-
-export default SearchBar
+};
