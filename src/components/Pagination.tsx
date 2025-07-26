648080c5c9b3740ee0329ff3c447af6df51d2c86
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-26 18:54:34 +0300

    refactor: convert Pagination to functional component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,44 +1,38 @@
-import { Component, ReactElement } from 'react';
+import { ReactNode } from 'react';
 import type { EmptyVoid, PaginationProps } from '@/types/types.ts';
 
-class Pagination extends Component<PaginationProps> {
-  handleNextPage: EmptyVoid = (): void => {
-    if (this.props.currentPage < this.props.totalPages) {
-      this.props.onPageChange(this.props.currentPage + 1);
+export default function Pagination({ currentPage, totalPages, onPageChange }: PaginationProps): ReactNode {
+  const handleNextPage: EmptyVoid = (): void => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
     }
-  }
+  };
 
-  handlePrevPage: EmptyVoid = (): void => {
-    if (this.props.currentPage > 1) {
-      this.props.onPageChange(this.props.currentPage - 1);
+  const handlePrevPage: EmptyVoid = (): void => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
     }
-  }
+  };
 
-  render(): ReactElement {
-    const { currentPage, totalPages } = this.props;
-
-    return (
-      <div className="flex justify-center my-4">
-        <button
-          onClick={this.handlePrevPage}
-          disabled={currentPage === 1}
-          className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-sky-600 rounded-lg hover:bg-sky-700 focus:ring-1 focus:outline-none focus:ring-sky-600 disabled:pointer-events-none cursor-pointer"
-        >
-          Prev
-        </button>
-        <span className="px-4 py-2">
-          Page {currentPage} of {totalPages}
-        </span>
-        <button
-          onClick={this.handleNextPage}
-          disabled={currentPage >= totalPages}
-          className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-sky-600 rounded-lg hover:bg-sky-700 focus:ring-1 focus:outline-none focus:ring-sky-600 disabled:pointer-events-none cursor-pointer"
-        >
-          Next
-        </button>
-      </div>
-    )
-  }
-}
-
-export default Pagination;
+  return (
+    <div className="flex justify-center my-4">
+      <button
+        onClick={handlePrevPage}
+        disabled={currentPage === 1}
+        className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-sky-600 rounded-lg hover:bg-sky-700 focus:ring-1 focus:outline-none focus:ring-sky-600 disabled:pointer-events-none cursor-pointer"
+      >
+        Prev
+      </button>
+      <span className="px-4 py-2">
+        Page {currentPage} of {totalPages}
+      </span>
+      <button
+        onClick={handleNextPage}
+        disabled={currentPage >= totalPages}
+        className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-sky-600 rounded-lg hover:bg-sky-700 focus:ring-1 focus:outline-none focus:ring-sky-600 disabled:pointer-events-none cursor-pointer"
+      >
+        Next
+      </button>
+    </div>
+  )
+};
