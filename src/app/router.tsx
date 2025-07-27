9acbebf5e76441c398782d732bab345b7a18971c
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-07-27 17:35:23 +0300

    feat: add about page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import MainLayout from '@/app/layout/MainLayout.tsx';
 import { ROUTES } from '@/app/routes.ts';
 import ErrorBoundary from '@/features/ErrorBoundary';
+import AboutPage from '@/pages/AboutPage.tsx';
 import HomePage from '@/pages/HomePage/HomePage';
 import NotFoundPage from '@/pages/NotFoundPage';
 
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
         path: ROUTES.HOME,
         element: <HomePage />,
         errorElement: <Navigate to={ROUTES.NOT_FOUND} replace />,
+      },
+      {
+        path: ROUTES.ABOUT,
+        element: <AboutPage />,
       },
       {
         path: '*',
